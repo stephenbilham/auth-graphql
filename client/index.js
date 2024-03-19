7962116ea67ed7e51aa3942278025e2f983b397a
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-03-19 14:33:30 -0700

    set up server routes and set up header and apollo

--- client/index.js
+++ client/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+
+import App from "./components/App";
+
+const client = new ApolloClient({
+	dataIdFromObject: (od) => od.id,
+});
 
 const Root = () => {
-  return (
-    <div>
-      Auth Starter
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Router history={hashHistory}>
+				<Route path="/" component={App}>
+					Home
+				</Route>
+			</Router>
+		</ApolloProvider>
+	);
 };
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+ReactDOM.render(<Root />, document.querySelector("#root"));
---
+++ server/schema/mutations.js
@@ -0,0 +1,42 @@
+const graphql = require("graphql");
+
+const UserType = require("./types/user_type");
+const AuthService = require("../services/auth");
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const mutation = new GraphQLObjectType({
+	name: "Mutation",
+	fields: {
+		signup: {
+			type: UserType,
+			args: {
+				email: { type: GraphQLString },
+				password: { type: GraphQLString },
+			},
+			resolve(parentValue, { email, password }, req) {
+				return AuthService.signup({ email, password, req });
+			},
+		},
+		signout: {
+			type: UserType,
+			resolve(parentValue, args, req) {
+				const { user } = req;
+				req.logout();
+				return user;
+			},
+		},
+		login: {
+			type: UserType,
+			args: {
+				email: { type: GraphQLString },
+				password: { type: GraphQLString },
+			},
+			resolve(parentValue, { email, password }, req) {
+				return AuthService.login({ email, password, req });
+			},
+		},
+	},
+});
+
+module.exports = mutation;
